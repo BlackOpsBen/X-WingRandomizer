383d4c6333788ab20027ee989be2daf6d7d015c9
Ben Mora <blackopsben@gmail.com>
2020-07-18 15:53:31 -0500

    about to try switching to product array instead of string array

--- Assets/DebugVerifyProducts.cs
+++ Assets/DebugVerifyProducts.cs
@@ -40,7 +40,7 @@ public class DebugVerifyProducts : MonoBehaviour
         for (int i = 0; i < items.Length; i++)
         {
             string[] productInclusions = items[i].GetProductsIncludedWith();
-            if (productInclusions != null)
+            if (productInclusions != null && productInclusions.Length > 0)
             {
                 for (int j = 0; j < productInclusions.Length; j++)
                 {
--- Assets/Product.cs
+++ Assets/Product.cs
@@ -3,5 +3,28 @@
 [CreateAssetMenu]
 public class Product : ScriptableObject
 {
-    
+    [SerializeField] private IComeInProducts[] itemsIncluded;
+
+    private void OnValidate()
+    {
+        foreach (IComeInProducts item in itemsIncluded)
+        {
+            string[] itemsProducts = item.GetProductsIncludedWith();
+            foreach (string product in itemsProducts)
+            {
+                if (product == this.name)
+                {
+                    Debug.Log(item.GetType() + " " + item.GetName() + " and " + this.GetType() + " " + this.name + " already reciprocate.");
+                }
+            }
+        }
+    }
 }
+
+
+/*
+ * if (item.GetName() == this.name)
+            {
+                Debug.Log(item.GetType() + " " + item.GetName() + " and " + this.GetType() + " " + this.name + " already reciprocate.");
+            }
+*/
