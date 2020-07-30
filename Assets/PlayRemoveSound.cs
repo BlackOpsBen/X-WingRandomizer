d4092220f327612c7046c4d87f36591d1cc4e57a
Ben Mora <blackopsben@gmail.com>
2020-07-30 15:10:37 -0500

    Added "Remove" sound

---
+++ Assets/PlayRemoveSound.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRemoveSound : MonoBehaviour
+{
+    public void PlayBeepSound()
+    {
+        AudioManager.Instance.Play("Remove");
+    }
+}
