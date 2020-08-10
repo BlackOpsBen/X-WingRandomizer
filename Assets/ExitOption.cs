89639c4ca8a3937610d31230220b77f1418ba40c
Ben Mora <blackopsben@gmail.com>
2020-08-09 22:49:45 -0500

    Added Exit option

---
+++ Assets/ExitOption.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitOption : MonoBehaviour
+{
+    [SerializeField] private GameObject exitDialogScreen;
+
+    private void Awake()
+    {
+        HideExitDialog();
+    }
+
+    public void ShowExitDialog()
+    {
+        exitDialogScreen.SetActive(true);
+    }
+
+    public void HideExitDialog()
+    {
+        exitDialogScreen.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
